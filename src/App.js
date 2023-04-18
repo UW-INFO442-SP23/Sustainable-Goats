c0324607f52d02b9ec7aef83169cce66988d9042
unknown <x2270trAdrowS@163.com>
2023-04-17 17:21:49 -0700

    basic map shown

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Map from './components/Map';
+import Test from './components/Test';
 
 function App(){
 
     return (
-        <div className='page-container'>
-            <div className='fill-content'>
-                <Routes>
-                    <Route path="/" element={
-                        <>
-                            <Map />
-                        </>
-                    } />
-                </Routes>
-            </div>
-        </div>
+
+       <Router>
+           <Routes>
+               <Route path={"/"} element={ <Map/> } />
+               <Route path={"/test"} element={<Test/> }/>
+           </Routes>
+       </Router>
+
     )
 }
+
+export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,24 @@
 import * as React from "react";
-import './leaflet.css';
-import './leaflet.js';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import "leaflet/dist/leaflet.css"
+import {Helmet} from "react-helmet"
+import "./Map.css"
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 
 const Map = () => {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} class="map">
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id={"map"}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
     )
 };
 export default Map;
