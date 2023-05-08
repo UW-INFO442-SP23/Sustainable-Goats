e90ad6cbcf05e02ceb888c8d5573ac7a44bcb61d
Emily Eckey <eeckey@uw.edu>
2023-05-08 15:22:29 -0700

    added parktopic logo and title to img folder

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -54,7 +54,7 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 /> */}   
-                <ReactLeafletGoogleLayer type={'roadmap'} />
+                <ReactLeafletGoogleLayer type={'hybrid'} />
                 {markers}
             </MapContainer>
     )
