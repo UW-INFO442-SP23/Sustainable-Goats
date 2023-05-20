87e0d2935c0b066a20d2aeb8699d5f6db191300f
unknown <x2270trAdrowS@163.com>
2023-05-20 16:26:31 -0700

    bug fixed

--- src/components/markersCard.js
+++ src/components/markersCard.js
@@ -4,28 +4,32 @@ import "./markersCard.css";
 
 const MarkersCard = (props) => {
     var features = props.feature;
+    console.log("passed features: " + features)
     let featurePills = [];
     var addy = props.address;
     var addyshort = addy.substring(0, addy.indexOf("("));
     for(var i = 0; i<features.length; i++){
-        if (features[i] == "Play Area" || features[i] == "Play Area (ADA Compliant)"){
+        if (features[i] === "Play Area" || features[i] === "Play Area (ADA Compliant)"){
             featurePills.push(
                 <p class="h5 pill-playarea">{features[i]}</p>
             )
         }
-        else if (features[i] == "Restrooms" || features[i] == "Restrooms (ADA Compliant)"){
+        else if (features[i] === "Restrooms" || features[i] === "Restrooms (ADA Compliant)"){
             featurePills.push(
                 <p class="h5 pill-restroom">{features[i]}</p>
             )
         }
-        else if (features[i] == "Paths" || features[i] == "Paths (ADA Compliant)"){
+        else if (features[i] === "Paths" || features[i] === "Paths (ADA Compliant)"){
             featurePills.push(
                 <p class="h5 pill-path">{features[i]}</p>
             )
         }
-        featurePills.push(
-            <p class="h5 card-pill">{features[i]}</p>
-        )
+        else {
+            featurePills.push(
+                <p class="h5 card-pill">{features[i]}</p>
+            )
+        }
+        
     }
     console.log(features)
 
