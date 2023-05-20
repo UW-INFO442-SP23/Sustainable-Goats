1eb76fd5afffb709810f96fd0b974a00f815fd3a
Delaney Edwards <delaney.l.edwards@gmail.com>
2023-05-20 13:51:28 -0700

    Adding color coded pills and fixing address string

--- src/components/markersCard.js
+++ src/components/markersCard.js
@@ -5,7 +5,24 @@ import "./markersCard.css";
 const MarkersCard = (props) => {
     var features = props.feature;
     let featurePills = [];
+    var addy = props.address;
+    var addyshort = addy.substring(0, addy.indexOf("("));
     for(var i = 0; i<features.length; i++){
+        if (features[i] == "Play Area" || features[i] == "Play Area (ADA Compliant)"){
+            featurePills.push(
+                <p class="h5 pill-playarea">{features[i]}</p>
+            )
+        }
+        else if (features[i] == "Restrooms" || features[i] == "Restrooms (ADA Compliant)"){
+            featurePills.push(
+                <p class="h5 pill-restroom">{features[i]}</p>
+            )
+        }
+        else if (features[i] == "Paths" || features[i] == "Paths (ADA Compliant)"){
+            featurePills.push(
+                <p class="h5 pill-path">{features[i]}</p>
+            )
+        }
         featurePills.push(
             <p class="h5 card-pill">{features[i]}</p>
         )
@@ -16,8 +33,8 @@ const MarkersCard = (props) => {
         <div class="card">
             <div class="card-body">
                 <h1 class="card-title">{props.title}</h1>
+                <p class="h5">{addyshort}</p>
                 <p class="h5">{props.hours}</p>
-                <p class="h5">{props.address}</p>
                 {featurePills}
             </div>
         </div>
